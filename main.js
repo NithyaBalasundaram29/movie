324416fb1b1c55b956aba69c0a079a287eaa64be
NithyaBalasundaram29 <nithyabalasundaram29@gmail.com>
2024-05-22 17:13:04 +0530

    router link

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 import express from 'express';
-
 const app=express()
 const port = 3000;
+
+
 app.get("/",(req,res)=>{
     res.json({"msg":"hello world"})
 });
+
+//crud of movie
+
+//R -reading movies
+app.get("/movies",()=>{
+
+})
+
+//C -CREATING movies
+app.post("/movies",()=>{
+
+})
+
+//U -UPDATING movies
+app.put("/movies/:id",()=>{
+
+})
+//D -deleting movies
+app.delete("/movies/:id",()=>{
+
+})
 app.listen(port,()=>{
     console.log("the server is running on locallost:", port);
 
